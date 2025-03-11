b48dbcc66c40c1f967276c703c01377503599a90
Adri <adri@B25>
2025-03-11 10:44:42 +0100

    LocalDate

--- src/main/java/org/ies/library/Main.java
+++ src/main/java/org/ies/library/Main.java
@@ -1,8 +1,17 @@
 package org.ies.library;
 
 
+import org.ies.library.model.BookLend;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-
+        var book = new BookLend(
+                "3432",
+                LocalDate.now(),
+                "143214X"
+        );
+        System.out.println(book);
     }
 }
