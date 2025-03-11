0b11917a560ed20b3919bfc67d9344fdba0aa63c
Adri <adri@B25>
2025-03-11 11:13:14 +0100

    LocalDate

--- src/main/java/org/ies/library/model/Library.java
+++ src/main/java/org/ies/library/model/Library.java
@@ -3,6 +3,7 @@ package org.ies.library.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeSet;
@@ -14,4 +15,15 @@ public class Library {
     private String name;
     private Map<String, Book> booksByIsbn;
     private List<Customer> customers;
-    private TreeSet<BookLend> bookLends;}
+    private TreeSet<BookLend> bookLends;
+
+    public static List<Book> getBook(List<Book> books, String genre){
+        List<Book> result = new ArrayList<>();
+        for (var book : books){
+            if (book.getGenres().contains(genre)){
+                result.add(book);
+            }
+        }
+        return result;
+    }
+}
