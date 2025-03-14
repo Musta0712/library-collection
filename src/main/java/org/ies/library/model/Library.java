71b87f21e6ba7ac9f8f0d1d8e2231ff76571c43e
Adri <adri@B25>
2025-03-14 13:00:48 +0100

    gitignore changed

--- src/main/java/org/ies/library/model/Library.java
+++ src/main/java/org/ies/library/model/Library.java
@@ -3,6 +3,7 @@ package org.ies.library.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -17,13 +18,37 @@ public class Library {
     private List<Customer> customers;
     private TreeSet<BookLend> bookLends;
 
-    public static List<Book> getBook(List<Book> books, String genre){
+    public static List<Book> getBook(List<Book> books, String genre) {
         List<Book> result = new ArrayList<>();
-        for (var book : books){
-            if (book.getGenres().contains(genre)){
+        for (var book : books) {
+            if (book.getGenres().contains(genre)) {
                 result.add(book);
             }
         }
         return result;
     }
+
+    public void addBookLend(String nif, String isbn) {
+        bookLends.add(
+                new BookLend(isbn, LocalDate.now(), nif)
+        );
+    }
+
+    public boolean removeGenre(String isbn, String genre) {
+        if (booksByIsbn.containsKey(isbn)) {
+            Book book = booksByIsbn.get(isbn);
+            return book.getGenres().remove(genre);
+        } else {
+            return false;
+        }
+    }
+
+    public boolean borrowedBook(String isbn, int customerNumber){
+
+    }
+
+    public void getGenre(String isbn){
+
+    }
+
 }
